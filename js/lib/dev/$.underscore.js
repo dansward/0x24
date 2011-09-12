9752291d99e0d6a6dc54e9b44dcf03520ed5572c
dan.s.ward <dan.s.ward@gmail.com>
2011-09-12 09:24:35 -0400

    moved underscore.js to cdn

--- js/lib/dev/$.underscore.js
+++ js/lib/dev/$.underscore.js
@@ -1,6 +1,6 @@
 $.require(
 	$.path + '$.underscore.js',
-	$.path + 'underscore.js',
+	'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.1.7/underscore-min.js',
 	function() {
 	_.each(_.functions($), function(v, k) {
 		_[k] = v;
